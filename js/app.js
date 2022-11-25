5e529237289507d16701848b01f5c2eb37843f65
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2022-11-25 11:47:51 +0400

    add selectors

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 'use strict';
+
+// SELECTORS
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
