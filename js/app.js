ee2c486f2bb03aa0f312492fa5c7bc7116b7aedf
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2022-11-26 22:32:50 +0400

    implement functionality for adding new todo

--- js/app.js
+++ js/app.js
@@ -4,3 +4,38 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+
+// FUNCTIONS
+const addTodo = event => {
+    // prevents the form from submitting
+    event.preventDefault();
+
+    // todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.textContent = todoInput.value;
+    newTodo.classList.add('todo-item');
+
+    todoDiv.append(newTodo);
+
+    // add check button
+    const checkBtn = document.createElement('button');
+    checkBtn.innerHTML = '<i class="fas fa-check"></i>';
+    checkBtn.classList.add('check-btn');
+    todoDiv.append(checkBtn);
+
+    // add delete button
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteBtn.classList.add('check-btn');
+    todoDiv.append(deleteBtn);
+
+    // attach the todo div to the list
+    todoList.append(todoDiv);
+};
+
+// EVENT LISTENERS
+todoButton.addEventListener('click', addTodo);
